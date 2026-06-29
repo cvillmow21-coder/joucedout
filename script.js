586f452fad5b438cfb4d643df13445514b0a9805
cvillmow21-coder <cvillmow21@gmail.com>
2026-06-29 12:33:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ window.placeOrder = function(){
         return;
     }
 
-    // Zufällige Bestellnummer
+    // Bestellnummer
     const orderId = "JUICE-" + Math.floor(10000 + Math.random() * 90000);
 
     // Datum
     const now = new Date();
     const date = now.toLocaleString("de-DE");
 
+    // Warenkorb leeren
     cart = [];
     localStorage.removeItem("cart");
     updateCart();
 
+    // Overlay
     const overlay = document.createElement("div");
 
     overlay.innerHTML = `
@@ -79,7 +81,6 @@ window.placeOrder = function(){
         location.href = "index.html";
     };
 };
-
     // Warenkorb speichern/clearen
     cart = [];
     localStorage.removeItem("cart");
